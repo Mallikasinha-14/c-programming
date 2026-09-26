2c97e8824f523e300f9710652a678a87f0e24a37
Mallikasinha-14 <mallika.sinha.cse.2025@miet.ac.in>
2026-09-26 21:39:23 +0530

    Create sll_deletion_first_26.09.26.c

---
+++ sll_deletion_first_26.09.26.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+struct node *new;
+struct node *temp;
+
+int main() {
+    int choice;
+    int after;
+
+    do {
+        new = (struct node*)malloc(sizeof(struct node));
+
+        printf("Enter element: ");
+        scanf("%d", &new->data);
+
+        new->next = NULL;
+
+        if (head == NULL) {
+            head = new;
+            temp = new;
+        }
+        else {
+            temp->next = new;
+            temp = new;
+        }
+
+        printf("Do you want to continue? (1 for Yes / 0 for No): ");
+        scanf("%d", &choice);
+
+    } while (choice == 1);
+
+
+    printf("\nOriginal Linked List: ");
+
+    temp = head;
+
+    while (temp != NULL) {
+        printf("%d  ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL");
+
+new  = (struct node*)malloc(sizeof(struct node));
+temp = head;
+head=head->next;
+free(temp);
+printf("\nlink list after deletion:");
+temp=head;
+while(temp!=NULL){
+printf("%d ",temp->data);
+temp = temp->next;
+}
+printf("NULL");
+
+    return 0;
+}
